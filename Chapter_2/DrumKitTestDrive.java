3708afe8f3574894e9a9864282710157638bad7b
MarcusCalgar <paraszthybalazs@gmail.com>
2020-11-23 14:08:30 +0100

    First files of chapter 2

---
+++ Chapter_2/DrumKitTestDrive.java
@@ -0,0 +1,28 @@
+d.playSnare();
+
+Drumkit d = new Drumkit();
+
+boolean topHat = true;
+boolean snare = true;
+
+void playSnare() {
+	System.out.println("bang bang ba-bang");
+}
+
+public static void main(String[] args){
+
+if(d.snare == true){
+d.playSnare();
+}
+
+d.snare = false;
+
+class DrumKitTestDrive {
+
+d.playTopHat();
+
+class Drumkit {
+
+void playTopHat(){
+	System.out.println("ding ding da-ding");
+}
