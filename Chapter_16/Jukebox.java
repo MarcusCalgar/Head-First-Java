19e61f1cfe92c0f1540de5b771754f088606f261
MarcusCalgar <paraszthybalazs@gmail.com>
2021-01-11 16:23:44 +0100

    Broken version, will be fixed soon

--- Chapter_16/Jukebox.java
+++ Chapter_16/Jukebox.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Jukebox {
 	
-	ArrayList<String> songList = new ArrayList<String>();
+	ArrayList<Song> songList = new ArrayList<Song>();
 	
 	public static void main(String[] args) {
 		new Jukebox().go();
@@ -12,11 +12,13 @@ public class Jukebox {
 	public void go() {
 		getSongs();
 		System.out.println(songList);
+		Collections.sort(songList);
+		System.out.println(songList);
 	}
 	
 	void getSongs() {
 		try {
-			File file = new File("SongList.txt");
+			File file = new File("SongListMore.txt");
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			String line = null;
 			while ((line = reader.readLine()) != null) {
@@ -29,6 +31,42 @@ public class Jukebox {
 	
 	void addSong(String lineToParse) {
 		String[] tokens = lineToParse.split("/");
-		songList.add(tokens[0]);
+		songList.add(new Song(tokens[0], tokens[1], tokens[2], tokens[3]));
 	}
 }
+
+class Song {
+	String title;
+	String artist;
+	String rating;
+	String bpm;
+	
+	Song(String t, String a, String r, String b) {
+		title = t;
+		artist = a;
+		rating = r;
+		bpm = b;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public String getRating() {
+		return rating;
+	}
+	
+	public String getBpm() {
+		return bpm;
+	}
+
+	public String toString() {
+		return title;
+	}
+}
+
+
